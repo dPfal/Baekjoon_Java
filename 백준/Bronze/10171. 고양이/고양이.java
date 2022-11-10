7b7f5342847d80ddc044ce2f1d4e206876f9f221
dPfal <100793454+dPfal@users.noreply.github.com>
2022-11-10 10:12:02 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String a = "\\    /\\\n"
+				+ " )  ( ')\n"
+				+ "(  /  )\n"
+				+ " \\(__)|" ;
+		System.out.println(a);
+		
+	}
+
+}
